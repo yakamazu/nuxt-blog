86684f3a39218e742ba11e5b0e10fe136fad8d0d
yakamazu <bonju2929@gmail.com>
2019-10-23 22:44:54 +0900

    modify firebase deploy

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -5,15 +5,15 @@
       <v-tabs vertical>
         <v-tab>
           <v-icon left>mdi-account</v-icon>
-          self‐introduction
+          about
         </v-tab>
         <v-tab>
           <v-icon left>mdi-thumb-up</v-icon>
           hobby             
         </v-tab>
         <v-tab>
-          <v-icon left>mdi-access-point</v-icon>
-          Option 3          
+          <v-icon left>mdi-rocket</v-icon>
+          will         
         </v-tab>
 
         <v-tab-item>
@@ -21,7 +21,7 @@
             <v-card-text>
               <p>福岡在住</p>
               <p>仕事ではDWHやBIに関連する泥臭いことがメイン</p>
-              <p>Webは素人なので、個人的に色々試してみたいことをメモしていく予定</p>
+              <p>個人的に色々試してみたいことをメモしていく</p>
             </v-card-text>
           </v-card>
         </v-tab-item>
@@ -30,7 +30,7 @@
             <v-card-text>
               <a :href="qiita.kyotei" target="_blank">競艇の自動売買</a><br>
               <p>
-                中々難しい。。。
+                試行錯誤中
               </p>
             </v-card-text>
           </v-card>
@@ -38,9 +38,15 @@
         <v-tab-item>
           <v-card flat>
             <v-card-text>
-              <p>
-                test3
-              </p>
+              <ul>
+                <li>Wind surfing</li>
+                <li>Fishing</li>
+                <li>Holding sushi with caught fish</li>
+                <li>Piano</li>
+                <li>English</li>
+                <li>Art</li>
+                <li>Martial arts</li>
+              </ul>
             </v-card-text>
           </v-card>
         </v-tab-item>
